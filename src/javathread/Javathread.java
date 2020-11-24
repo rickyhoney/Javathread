719d61d832d81a4312ab9401bf372893afdc5233
miele.riccardo <miele.riccardo@TAVE-PC28.OSDBSESTO.local>
2020-11-24 13:31:10 +0100

    finish

--- src/javathread/Javathread.java
+++ src/javathread/Javathread.java
@@ -15,7 +15,12 @@ public class Javathread {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+    Processo T1 = new Processo("T1");
+    Processo T2 = new Processo("T2");
     
+    T1.start();
+    T2.start();
+    
+    }  
 }
---
+++ src/javathread/Processo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author miele.riccardo
+ */
+public class Processo extends Thread {
+    String g;
+    
+    public Processo(String g) {
+        this.g = g;
+    }
+
+    public void run(){
+        for(;;) System.out.println(g);
+    }
+    
+
+}
